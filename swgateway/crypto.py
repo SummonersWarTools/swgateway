bcb0815e7bf6c5d0c510106524e151957a577735
Ziddia <joshua.barton.smith12@gmail.com>
2022-11-22 06:02:49 -0500

    fixup crypto to support decompressed data

--- swgateway/crypto.py
+++ swgateway/crypto.py
@@ -7,21 +7,19 @@ class CryptoParams:
     def __init__(self, key, iv):
         self.__key = key
         self.__iv = iv
-        self.__crypto = AES.new(self.__key, AES.MODE_CBC, iv=self.__iv)
 
     def GetCrypto(self):
-        return self.__crypto
+        return AES.new(self.__key, AES.MODE_CBC, iv=self.__iv)
 
 class SWCryptoMgr:
 
     DEFAULT = CryptoParams(b'Gr4S2eiNl7zq5MrU', b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
+    CHAT = CryptoParams(b'E2wN5Eo0t4gle92Z', b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
 
-    def Decrypt(cryptoParams, encryptedContent):
-        return decompress(SWCryptoMgr.__WithRecrypto(unpad(cryptoParams.GetCrypto().decrypt(encryptedContent), AES.block_size))).decode('utf-8')
+    def Decrypt(cryptoParams, encryptedContent, compression = True):
+        decrypted = unpad(cryptoParams.GetCrypto().decrypt(encryptedContent), AES.block_size)
+        if compression: return decompress(decrypted).decode('utf-8')
+        else: return decrypted.decode('utf-8')
 
     def Encrypt(cryptoParams, plaintextContent):
-        return SWCryptoMgr.__WithRecrypto(cryptoParams.GetCrypto().encrypt(pad(plaintextContent, AES.block_size)))
-
-    def __WithRecrypto(param):
-        SWCryptoMgr.DEFAULT = CryptoParams(b'Gr4S2eiNl7zq5MrU', b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
-        return param
+        return cryptoParams.GetCrypto().encrypt(pad(plaintextContent, AES.block_size))
